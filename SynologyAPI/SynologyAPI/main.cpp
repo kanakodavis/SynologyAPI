418cbcf21fdfad087b0a4ea059b1da8353610ae6
Deus Ex Machina <matthias.gusenbauer@palladion.it>
2014-09-26 22:19:10 +0200

    [INITIAL COMMIT] added some .gitignore

---
+++ SynologyAPI/SynologyAPI/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SynologyAPI
+//
+//  Created by Matthias Gusenbauer on 26/09/14.
+//  Copyright (c) 2014 palladion.it. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
