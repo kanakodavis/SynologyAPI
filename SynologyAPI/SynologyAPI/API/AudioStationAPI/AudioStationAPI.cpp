7077712bf26277620074930b82b59854da996296
Deus Ex Machina <matthias.gusenbauer@palladion.it>
2014-10-06 00:46:31 +0200

    [CLEANUP] minor cleanup

--- SynologyAPI/SynologyAPI/API/AudioStationAPI/AudioStationAPI.cpp
+++ SynologyAPI/SynologyAPI/API/AudioStationAPI/AudioStationAPI.cpp
@@ -4,7 +4,6 @@
 //
 
 #include "AudioStationAPI.h"
-#include <thread>
 
 namespace api {
     
--- SynologyAPI/SynologyAPI/API/AudioStationAPI/AudioStationAPI.h
+++ SynologyAPI/SynologyAPI/API/AudioStationAPI/AudioStationAPI.h
@@ -8,6 +8,7 @@
 
 #include <stdio.h>
 #include <vector>
+#include <thread>
 
 #include "AuthAPI.h"
 #include "ASParser.h"
